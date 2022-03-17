1a5b30c909fb392918c9acf550997e1c5f224b1a
AravaaK <98435333+AravaaK@users.noreply.github.com>
2022-03-17 15:14:57 -0400

    Updated JS file
    Defining apiKey and apiUrl
    Added showTemperature function

--- src/script.js
+++ src/script.js
@@ -1,1 +1,10 @@
+function showTemperature(response) {
+  let city = document.querySelector("#city");
+  city.innerHTML = response.data.name;
+  let temp = document.querySelector("#label");
+  temp.innerHTML = Math.round(response.data.main.temp);
+}
+let apiKey = "473f4a214744fd25fcd4c7f9127f6534";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
 
+axios.get(apiUrl).then(showTemperature);
