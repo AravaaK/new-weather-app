559653408c5b615e9f9169a17114810916b1e3a7
AravaaK <98435333+AravaaK@users.noreply.github.com>
2022-03-20 23:59:13 -0400

    Updated showTemperature function
    Added id of "description"

--- src/script.js
+++ src/script.js
@@ -1,10 +1,13 @@
 function showTemperature(response) {
+  console.log(response.data.weather[0].description);
   let city = document.querySelector("#city");
   city.innerHTML = response.data.name;
   let temp = document.querySelector("#label");
   temp.innerHTML = Math.round(response.data.main.temp);
+  let description = document.querySelector("#description");
+  description.innerHTML = response.deta.weather[0].description;
 }
 let apiKey = "473f4a214744fd25fcd4c7f9127f6534";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(showTemperature);
