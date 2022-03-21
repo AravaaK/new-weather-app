b6851c6a99bfac42f45dffd9072282c55b321939
AravaaK <98435333+AravaaK@users.noreply.github.com>
2022-03-21 09:10:55 -0400

    Updated date & time
    Added formatDate function

--- src/script.js
+++ src/script.js
@@ -1,4 +1,22 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day}, ${hours}:${minutes}`;
+}
+
 function showTemperature(response) {
+  console.log(response.data.dt);
   let city = document.querySelector("#city");
   city.innerHTML = response.data.name;
   let temp = document.querySelector("#label");
@@ -8,11 +26,13 @@ function showTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   humidityElement.innerHTML = response.data.main.humidity;
   let feelsLikeElement = document.querySelector("#feelsLike");
-  feelsLikeElement.innerHTML = response.data.main.feels_like;
+  feelsLikeElement.innerHTML = Math.round(response.data.main.feels_like);
   let windSpeedElement = document.querySelector("#windSpeed");
   windSpeedElement.innerHTML = Math.round(response.data.wind.speed);
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 let apiKey = "473f4a214744fd25fcd4c7f9127f6534";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(showTemperature);
